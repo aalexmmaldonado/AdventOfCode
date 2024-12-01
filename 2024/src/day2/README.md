d88c949e661b227a13060715651f670861f65006
Alex Maldonado <alex@oasci.org>
2024-12-01 18:27:25 -0500

    Setup Day 2

---
+++ 2024/src/day2/README.md
@@ -0,0 +1,3 @@
+# Day 2
+
+[Problem statement](https://adventofcode.com/2024/day/2)
--- 2024/src/main.rs
+++ 2024/src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
 
 mod day1;
+mod day2;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -10,6 +11,12 @@ fn main() {
         Some("1") => {
             if let Err(e) = day1::run() {
                 eprintln!("Error running Day 1: {}", e);
+                std::process::exit(1);
+            }
+        }
+        Some("2") => {
+            if let Err(e) = day2::run() {
+                eprintln!("Error running Day 2: {}", e);
                 std::process::exit(1);
             }
         }
