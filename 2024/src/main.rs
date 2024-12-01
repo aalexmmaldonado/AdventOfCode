f6e7f6a0736a0382af30b75fde59c75650dc879e
Alex Maldonado <alex@oasci.org>
2024-12-01 01:01:05 -0500

    Setup aoc2024

--- 2024/src/main.rs
+++ 2024/src/main.rs
@@ -1,3 +1,14 @@
+use std::env;
+
+mod day1;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let day_selection = args.get(1).map(String::as_str);
+    match day_selection {
+        Some("1") => day1::run(),
+        Some(_) => println!("Invalid day!"),
+        None => println!("No day specified!"),
+    }
 }
