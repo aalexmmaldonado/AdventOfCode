c8ddfb22c584aa26c13941f5903dfa3a51568bc3
Alex Maldonado <alex@oasci.org>
2024-12-04 01:01:01 -0500

    Finish d1p1

--- 2024/src/day4/main.rs
+++ 2024/src/day4/main.rs
@@ -1,11 +1,20 @@
 use std::error::Error;
 use std::fs;
 
+use strum::IntoEnumIterator;
+use strum_macros::EnumIter;
+
 pub fn run() -> Result<(), Box<dyn Error>> {
     println!("Running Day 4!");
 
     let path_instructions = "./src/day4/input.txt";
+    let word = String::from("XMAS");
     let contents = read_file(&path_instructions)?;
+    let grid = to_grid(&contents);
+    let word_locs = search_grid_for_word(&grid, &word);
+    let n_word_found = word_locs.len();
+    assert_eq!(n_word_found, 2575);
+    println!("Times '{}' was found: {:?}", word, n_word_found);
 
     Ok(())
 }
@@ -16,4 +25,116 @@ fn read_file(path: &str) -> Result<String, Box<dyn Error>> {
         fs::read_to_string(path).map_err(|e| format!("Unable to read file {}: {}", path, e))?;
 
     Ok(contents)
+}
+
+fn to_grid(contents: &String) -> Vec<Vec<&str>> {
+    let grid: Vec<Vec<&str>> = contents
+        .lines()
+        .map(|line| line.split("").filter(|&s| !s.is_empty()).collect())
+        .collect();
+    grid
+}
+
+#[derive(Debug, EnumIter)]
+enum GridDirection {
+    Up,
+    Down,
+    Left,
+    Right,
+    UpLeft,
+    UpRight,
+    DownLeft,
+    DownRight,
+}
+
+impl GridDirection {
+    fn delta(&self) -> (isize, isize) {
+        match self {
+            GridDirection::Up => (-1, 0),
+            GridDirection::Down => (1, 0),
+            GridDirection::Left => (0, -1),
+            GridDirection::Right => (0, 1),
+            GridDirection::UpLeft => (-1, -1),
+            GridDirection::UpRight => (-1, 1),
+            GridDirection::DownLeft => (1, -1),
+            GridDirection::DownRight => (1, 1),
+        }
+    }
+}
+
+fn traverse_grid(
+    start: (usize, usize),
+    direction: &GridDirection,
+    rows: usize,
+    cols: usize,
+    step: usize,
+) -> Option<(usize, usize)> {
+    let (row, col) = start;
+    let (delta_row, delta_col) = direction.delta();
+
+    // Calculate new position
+    let mut new_row = row as isize;
+    let mut new_col = col as isize;
+    for _ in 0..step {
+        new_row = new_row + delta_row;
+        new_col = new_col + delta_col;
+    }
+
+    // Check bounds
+    if new_row >= 0 && new_row < rows as isize && new_col >= 0 && new_col < cols as isize {
+        Some((new_row as usize, new_col as usize))
+    } else {
+        None // Out of bounds
+    }
+}
+
+fn check_for_word(
+    word: &str,
+    start: (usize, usize),
+    direction: &GridDirection,
+    grid: &Vec<Vec<&str>>,
+) -> bool {
+    let (row, col) = start;
+    let rows = grid.len();
+    let cols = grid[0].len();
+
+    let letters: Vec<&str> = word.split("").filter(|&s| !s.is_empty()).collect();
+
+    // Check if starting position matches
+    if grid[row][col] != letters[0] {
+        return false;
+    }
+
+    for i_letter in 1..letters.len() {
+        let next_loc = traverse_grid(start, direction, rows, cols, i_letter);
+        match next_loc {
+            None => return false,
+            Some(next_loc) => {
+                let (row_next, col_next) = next_loc;
+                if grid[row_next][col_next] != letters[i_letter] {
+                    return false;
+                }
+            }
+        }
+    }
+
+    true
+}
+
+fn search_grid_for_word(grid: &Vec<Vec<&str>>, word: &str) -> Vec<(usize, usize, GridDirection)> {
+    let mut word_locs: Vec<(usize, usize, GridDirection)> = Vec::new();
+    let rows = grid.len();
+    let cols = grid[0].len();
+
+    for i in 0..rows {
+        for j in 0..cols {
+            for direction in GridDirection::iter() {
+                if check_for_word(word, (i, j), &direction, grid) {
+                    word_locs.push((i, j, direction));
+                }
+            }
+        }
+    }
+
+    word_locs
 }
