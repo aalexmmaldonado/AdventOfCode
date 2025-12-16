1b51e5dc20e55f98bb2b1952e40e12d5ff10cbd2
Alex Maldonado <alex@scient.ing>
2025-12-15 19:27:57 -0500

    Get ready for aoc25

---
+++ 25/src/day00/README.md
@@ -0,0 +1,3 @@
+# Day 00
+
+[Problem statement](https://adventofcode.com/2025/)
---
+++ 25/src/main.rs
@@ -0,0 +1,23 @@
+use std::env;
+
+mod day00;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let day_selection = args.get(1).map(String::as_str);
+    match day_selection {
+        Some("00") => {
+            if let Err(e) = day00::run() {
+                eprintln!("Error running Day 00: {}", e);
+                std::process::exit(1);
+            }
+        }
+        Some(_) => {
+            println!("Invalid day!");
+        }
+        None => {
+            println!("No day specified!");
+        }
+    }
+}
