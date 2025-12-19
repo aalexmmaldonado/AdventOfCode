b94f94151476ce5aa46f666e3502a3cf9d783748
Alex Maldonado <alex@scient.ing>
2025-12-18 22:25:37 -0500

    Refactor aoc25 d3p1

--- 25/src/day03/main.rs
+++ 25/src/day03/main.rs
@@ -6,9 +6,10 @@ pub fn run() -> Result<(), Box<dyn Error>> {
 
     let input: String = fs::read_to_string("./src/day03/input.txt")?;
 
-    let (banks, max_voltage): (Vec<Bank>, u64) = parse_banks(&input)?;
-    assert_eq!(max_voltage, 17554);
-    println!("{:?}", &max_voltage);
+    let banks: Vec<Bank> = parse_banks(&input)?;
+    let max_total_voltage_2 = get_max_total_voltage(&banks, 2);
+    assert_eq!(max_total_voltage_2, 17554);
+    println!("Part 1: {:?}", &max_total_voltage_2);
     Ok(())
 }
 
@@ -21,22 +22,10 @@ struct Voltage {
 #[derive(Debug)]
 struct Bank {
     batts: Vec<u8>,
-    most_sig_dig: Voltage,
-    least_sig_dig: Voltage,
 }
 
-impl Bank {
-    fn combined_voltage(&self) -> u64 {
-        let msd = self.most_sig_dig.val;
-        let lsd = self.least_sig_dig.val;
-
-        (msd as u64 * 10) + (lsd as u64)
-    }
-}
-
-fn parse_banks(contents: &str) -> Result<(Vec<Bank>, u64), Box<dyn Error>> {
+fn parse_banks(contents: &str) -> Result<Vec<Bank>, Box<dyn Error>> {
     let mut banks: Vec<Bank> = vec![];
-    let mut max_voltage: u64 = 0;
 
     for line in contents.lines() {
         let voltages: Vec<u8> = line
@@ -45,34 +34,67 @@ fn parse_banks(contents: &str) -> Result<(Vec<Bank>, u64), Box<dyn Error>> {
             .map(|x| x as u8)
             .collect();
 
-        let (max_msd_idx, max_msd_val) = voltages[..voltages.len() - 1]
-            .iter()
-            .enumerate()
-            .max_by(|a, b| a.1.cmp(&b.1).then_with(|| b.0.cmp(&a.0)))
-            .map(|(i, &v)| (i, v))
-            .unwrap();
-        let (max_lsd_idx, max_lsd_val) = voltages
-            .iter()
-            .enumerate()
-            .skip(max_msd_idx + 1)
-            .max_by_key(|&(i, &v)| v)
-            .map(|(i, &v)| (i, v))
-            .unwrap();
-
-        let bank = Bank {
-            batts: voltages,
-            most_sig_dig: Voltage {
-                val: max_msd_val,
-                pos: max_msd_idx as u64,
-            },
-            least_sig_dig: Voltage {
-                val: max_lsd_val,
-                pos: max_lsd_idx as u64,
-            },
-        };
-        max_voltage += bank.combined_voltage();
+        let bank = Bank { batts: voltages };
         banks.push(bank);
     }
 
-    Ok((banks, max_voltage))
+    Ok(banks)
+}
+
+fn find_max_bank_voltage(banks: &[Bank], n_batteries_use: u64) -> Vec<Vec<Voltage>> {
+    let mut bank_voltages: Vec<Vec<Voltage>> = vec![];
+    for bank in banks {
+        let n_batts = bank.batts.len();
+        let mut start_idx: usize = 0;
+        let mut stop_idx = n_batts - (n_batteries_use - 1) as usize;
+
+        let mut voltages: Vec<Voltage> = vec![];
+        for _ in 0..n_batteries_use {
+            let max_idx: usize;
+            let max_val: u8;
+            if (start_idx != stop_idx) && (start_idx != n_batts - 1 as usize) {
+                (max_idx, max_val) = bank.batts[..stop_idx]
+                    .iter()
+                    .enumerate()
+                    .skip(start_idx)
+                    .max_by(|a, b| a.1.cmp(&b.1).then_with(|| b.0.cmp(&a.0)))
+                    .map(|(i, &v)| (i, v))
+                    .unwrap();
+            } else {
+                max_idx = start_idx;
+                max_val = bank.batts[start_idx];
+            }
+
+            voltages.push(Voltage {
+                val: max_val,
+                pos: max_idx as u64,
+            });
+            start_idx = max_idx + 1;
+            stop_idx += 1;
+        }
+        bank_voltages.push(voltages);
+    }
+
+    bank_voltages
+}
+
+fn get_bank_voltage(voltages: &[Voltage]) -> u64 {
+    let mut mult_factor: u64 = 10u64.pow(voltages.len() as u32 - 1);
+    let mut total_voltage: u64 = 0;
+
+    for volt in voltages {
+        total_voltage += mult_factor * volt.val as u64;
+        mult_factor /= 10;
+    }
+
+    total_voltage
+}
+
+fn get_max_total_voltage(banks: &[Bank], n_batteries_use: u64) -> u64 {
+    let total_voltage = find_max_bank_voltage(banks, n_batteries_use)
+        .into_iter()
+        .map(|x| get_bank_voltage(&x))
+        .sum();
+
+    total_voltage
 }
